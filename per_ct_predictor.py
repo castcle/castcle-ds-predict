8db443d4978645f257d4fb04cb293274812f2d4a
Navavat Pipatsart <pnastra_castcle@Navavats-MBP.local>
2021-11-16 19:09:01 +0700

    1st try running

--- per_ct_predictor.py
+++ per_ct_predictor.py
@@ -2,13 +2,37 @@ import json
 from mongo_client import ping_mongodb, mongo_client
 
 
+#! simulate mocked event
+## following creator's content
+event = {
+    'userId': '6170067a51db852fb36d2109',
+    'contents': [
+        '6188a77b1ec609099d728303',
+        '6188ed841ec60953167283f4'
+    ]
+}
+
 def handle(event, context):
     if event.get("source") == "serverless-plugin-warmup":
         ping_mongodb()
         print("WarmUp - Lambda is warm!")
         return
 
-    print(json.dumps(event['body'], indent=4))
+    # print(json.dumps(event['body'], indent=4))
+
+    from modules.personalized_content.personalize_content_predictor import personalized_content_predict_main
+
+    personalized_content_predict_main(event,
+                                      src_database_name = 'analytics-db',
+                                      src_collection_name = 'mlArtifacts',
+                                      analytics_db = 'analytics-db',
+                                      creator_stats_collection = 'creatorStats',
+                                      content_stats_collection = 'contentStats',
+                                      dst_database_name = 'analytics-db',
+                                      dst_collection_name = 'feedItems_test',
+                                      model_name = 'xgboost')
+
+    print('prediction of content id: ', event.get('userId', None),' completed')
 
     return {
         'statusCode': 200,
