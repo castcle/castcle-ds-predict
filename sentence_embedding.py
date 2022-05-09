2f9ad213c012bc15b9f730678372ae9a84cf89e0
narumitki <96113461+narumitki@users.noreply.github.com>
2022-05-09 21:34:29 +0700

    download SentenceTransformer.pkl

--- sentence_embedding.py
+++ sentence_embedding.py
@@ -2,6 +2,21 @@ import json
 from mongo_client import ping_mongodb
 import datetime
 
+def download_model():
+    BUCKET_NAME = 'ml-dev.castcle.com'
+    file_path = 'sentence_transformer/SentenceTransformer.pkl'
+    file_name = 'SentenceTransformer.pkl'
+    
+    s3_client = boto3.client('s3')
+    s3_client.download_file(BUCKET_NAME, file_path, file_name)
+    print('Sucessfull_download:',file_name)
+    
+    from os import listdir
+    onlyfiles = [f for f in listdir('./')]
+    print('file_list :', onlyfiles)
+
+# download SentenceTransformer.pkl model
+download_model()
 
 def handle(event, context):
     if event.get("source") == "serverless-plugin-warmup":
